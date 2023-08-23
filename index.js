b8845ddba47b0fda9d119eaa15fe8b07bfd0f828
Arslan Majeed <arslanmajeed214@gmail.com>
2023-08-24 03:24:43 +0500

    Use luxon library to show date and time

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 import { navConfiguration } from "./modules/navigationBar.js";
 import { Library } from "./modules/booksLibrary.js";
+import { showTime } from "./modules/displayDate.js";
 document.addEventListener("DOMContentLoaded", () => {
+  const booksCollection = new Library();
+  booksCollection.initializeLocalStorage();
+
   navConfiguration();
+
+  const addBtn = document.getElementById("addBtn");
+  addBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const titleInput = document.getElementById("title");
+    const authorInput = document.getElementById("author");
+    booksCollection.addData(titleInput.value, authorInput.value);
+    titleInput.value = "";
+    authorInput.value = "";
+  });
+
+  booksCollection.showBooks();
 });
+
+// Displaying time using luxon library. 
+showTime();
+setInterval(showTime, 1000);
--- modules/booksLibrary.js
+++ modules/booksLibrary.js
@@ -87,21 +87,4 @@ class Library {
   }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  const booksCollection = new Library();
-  booksCollection.initializeLocalStorage();
-
-  const addBtn = document.getElementById("addBtn");
-  addBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    const titleInput = document.getElementById("title");
-    const authorInput = document.getElementById("author");
-    booksCollection.addData(titleInput.value, authorInput.value);
-    titleInput.value = "";
-    authorInput.value = "";
-  });
-
-  booksCollection.showBooks();
-});
-
-export { Library }
+export { Library };
